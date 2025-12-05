c57a2fb2f744ab4697dfa4e47bc3c521e8e67525
hayamij <phuongtuan.dev@gmail.com>
2025-12-05 11:21:48 +0700

    clean compile and test

--- src/main/java/com/motorbike/business/usecase/control/GetAllAccessoriesUseCaseControl.java
+++ src/main/java/com/motorbike/business/usecase/control/GetAllAccessoriesUseCaseControl.java
@@ -6,7 +6,6 @@ import com.motorbike.business.ports.repository.ProductRepository;
 import com.motorbike.business.usecase.input.GetAllAccessoriesInputBoundary;
 import com.motorbike.business.usecase.output.GetAllAccessoriesOutputBoundary;
 import com.motorbike.domain.entities.PhuKienXeMay;
-import com.motorbike.domain.entities.SanPham;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,18 +24,13 @@ public class GetAllAccessoriesUseCaseControl implements GetAllAccessoriesInputBo
     }
 
     @Override
-<<<<<<< HEAD
     public void execute() {
-=======
-    public void execute(Void inputData) {
->>>>>>> acbf7c86c2b72da9877419822177e0ede7261061
         GetAllAccessoriesOutputData outputData = null;
         Exception errorException = null;
 
         try {
             List<PhuKienXeMay> all = productRepository.findAllAccessories();
 
-<<<<<<< HEAD
             List<AccessoryItem> items = all.stream()
                     .map(pk -> new AccessoryItem(
                             pk.getMaSanPham(),
@@ -53,28 +47,6 @@ public class GetAllAccessoriesUseCaseControl implements GetAllAccessoriesInputBo
                     .collect(Collectors.toList());
 
             outputData = new GetAllAccessoriesOutputData(items);
-=======
-            List<AccessoryItem> accessories = allProducts.stream()
-                    .filter(p -> p instanceof PhuKienXeMay)
-                    .map(p -> {
-                        PhuKienXeMay pk = (PhuKienXeMay) p;
-                        return new AccessoryItem(
-                                pk.getMaSanPham(),
-                                pk.getTenSanPham(),
-                                pk.getMoTa(),
-                                pk.getGia(),
-                                pk.getSoLuongTonKho(),
-                                pk.getHinhAnh(),
-                                pk.getLoaiPhuKien(),
-                                pk.getThuongHieu(),
-                                pk.getChatLieu(),
-                                pk.getKichThuoc()
-                        );
-                    })
-                    .collect(Collectors.toList());
-
-            outputData = new GetAllAccessoriesOutputData(accessories);
->>>>>>> acbf7c86c2b72da9877419822177e0ede7261061
         } catch (Exception e) {
             errorException = e;
         }
